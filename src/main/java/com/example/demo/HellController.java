4e1425e26be47e5c0843e5e0dced82ffd9341066
zff <zhaofeifei@shougang.com.cn>
2018-10-21 17:16:47 +0800

    first commit

---
+++ src/main/java/com/example/demo/HellController.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HellController {
+
+    @RequestMapping("/hello")
+    public String say(){
+        return "Hello SpringBoot!";
+    }
+}
